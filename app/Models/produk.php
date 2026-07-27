08f9a55cc3bbbeb479df012c3607a1b4ba9dd2ba
zahwa-rusdiana <pppprusdiana@gmail.com>
2026-07-27 08:47:04 +0700

    Revert "update model produk"
    This reverts commit 4729d5b5a5758006d831a57c07a48918930ab13a.

--- app/Models/produk.php
+++ app/Models/produk.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-     
+     protected $table = 'products';
+
+    protected $fillable = [
+        'nama',
+        'harga',
+        'stok'
+    ];
 }
